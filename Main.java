464b642d945edf990af7448a3a2f3506c70da6f8
Eliea06 <105725262+Eliea06@users.noreply.github.com>
2022-05-17 09:28:12 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 public class Main
 {
-  String[] names = {"Maddy", "Connor", "Sinead", "Mallory", "Amy"};
+  ArrayList <String> names = new ArrayList <String>();
+  
+  names.add("Maddy");
+  names.add("Mallory");
+  names.add("Sinead");
+  names.add("Connor");
+  names.add("Amy");
+  
+  TeamOne one = TeamOne(names);
+  
+  one.getTeamOne();
+  
 }
